5fa5dca4d08124174951ea216c1e2b5a488c2571
dhiguero <daniel.higuero@gmail.com>
2017-04-20 15:00:44 +0200

    Add new cut information for larger alphabets

--- src/main/scala/eu/proteus/solma/sax/Cuts.scala
+++ src/main/scala/eu/proteus/solma/sax/Cuts.scala
@@ -34,7 +34,6 @@ package eu.proteus.solma.sax
  * [-Inf, -0.6744898), [-0.6744898, 0), [0, 0.6744898), [0.6744898, Inf). Based on those ranges,
  * we next calculate the distance matrix between the ranges.
  */
-// Deactivate the magic number check as this class contains raw numbers.
 object Cuts{
 
   /**
@@ -49,6 +48,51 @@ object Cuts{
   private val Cuts2 : Array[Double] = Array(0.0d)
 
   /**
+   * Breakpoints for 3 areas.
+   */
+  private val Cuts3 : Array[Double] = Array(-0.4307273, 0.4307273)
+
+  /**
+   * Breakpoints for 4 areas.
+   */
+  private val Cuts4 : Array[Double] = Array(-0.6744898, 0.0000000, 0.6744898)
+
+  /**
+   * Breakpoints for 5 areas.
+   */
+  private val Cuts5 : Array[Double] = Array(-0.8416212, -0.2533471, 0.2533471, 0.8416212)
+
+  /**
+   * Breakpoints for 6 areas.
+   */
+  private val Cuts6 : Array[Double] = Array(-0.9674216, -0.4307273, 0.0000000, 0.4307273,
+    0.9674216)
+
+  /**
+   * Breakpoints for 7 areas.
+   */
+  private val Cuts7 : Array[Double] = Array(-1.0675705, -0.5659488, -0.1800124, 0.1800124,
+    0.5659488, 1.0675705)
+
+  /**
+   * Breakpoints for 8 areas.
+   */
+  private val Cuts8 : Array[Double] = Array(-1.1503494, -0.6744898, -0.3186394, 0.0000000,
+    0.3186394, 0.6744898, 1.1503494)
+
+  /**
+   * Breakpoints for 9 areas.
+   */
+  private val Cuts9 : Array[Double] = Array(-1.2206403, -0.7647097, -0.4307273, -0.1397103,
+    0.1397103, 0.4307273, 0.7647097, 1.2206403)
+
+  /**
+   * Breakpoints for 10 areas.
+   */
+  private val Cuts10 : Array[Double] = Array(-1.2815516, -0.8416212, -0.5244005, -0.2533471,
+    0.0000000, 0.2533471,  0.5244005, 0.8416212, 1.2815516)
+
+  /**
    * Distances for 2 areas.
    */
   private val Distances2 : Array[Array[Double]] = Array(Array(0.0d, 0.0d), Array(0.0d, 0.0d))
@@ -61,6 +105,14 @@ object Cuts{
   def breakpoints(numberCuts: Int) : Array[Double] = {
     numberCuts match {
       case 2 => Cuts.Cuts2
+      case 3 => Cuts.Cuts3
+      case 4 => Cuts.Cuts4
+      case 5 => Cuts.Cuts5
+      case 6 => Cuts.Cuts6
+      case 7 => Cuts.Cuts7
+      case 8 => Cuts.Cuts8
+      case 9 => Cuts.Cuts9
+      case 10 => Cuts.Cuts10
       case _ => throw new RuntimeException("Number of cuts not supported")
     }
   }
