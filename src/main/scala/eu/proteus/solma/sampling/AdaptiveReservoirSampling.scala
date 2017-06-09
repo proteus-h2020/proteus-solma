b06de3ece931a8edd10dd54f7a0eb2ec51f99343
saadm2 <smohamad@bournemouth.ac.uk>
2017-06-09 11:28:22 +0100

    Creating case class instead of too big tuples

--- src/main/scala/eu/proteus/solma/sampling/AdaptiveReservoirSampling.scala
+++ src/main/scala/eu/proteus/solma/sampling/AdaptiveReservoirSampling.scala
@@ -17,6 +17,7 @@ import scala.util.Random
 
 
 
+
 @Proteus
 class AdaptiveReservoirSampling extends  StreamTransformer[AdaptiveReservoirSampling]{
   import AdaptiveReservoirSampling._
@@ -61,9 +62,9 @@ object AdaptiveReservoirSampling{
   }
 
   //=====================================Extra=================================================
-  def factorial2(n:Int,result:Int):Int = if (n==0) result else factorial2(n-1,n*result)
-  def factorial(n: Int): Int = factorial2(n,1)
+  def factorial(n: Int): Int = {   if (n == 0) 1 else n * factorial(n-1)  }
   def min_m (i: Int,ReservoirSize: Int,delta: Int,threshold: Double) : Int = {
+
     var m = delta
     var UC = 0.0
     var x = ReservoirSize
@@ -81,13 +82,15 @@ object AdaptiveReservoirSampling{
     }
     m
   }
+  case class inp[T](var streamCounter1:Long,var streamCounter2:Long, var streamCounter3:Long,
+                    var adaptedReserS1:mutable.ArrayBuffer[T], var adaptedReserS2:mutable.ArrayBuffer[T],
+                    var adaptivereservoir:mutable.ArrayBuffer[T])
 
   // ==================================== Factory methods ==========================================
 
   def apply(): AdaptiveReservoirSampling = {
     new AdaptiveReservoirSampling()
   }
-
 
   // ==================================== Operations ==========================================
 
@@ -117,81 +120,79 @@ object AdaptiveReservoirSampling{
         val thresh=resultingParameters(AdaptiveReservoirThreshold)
         val changedReservoirSize = resultingParameters(Change)
         val gen = new XORShiftRandom()
-        implicit val typeInfo = createTypeInformation[(Long,Long,Long,mutable.ArrayBuffer[T],
-                                                       mutable.ArrayBuffer[T], mutable.ArrayBuffer[T])])
+        implicit val typeInfo = createTypeInformation[inp[T]]
         implicit val retInfo = createTypeInformation[mutable.ArrayBuffer[T]]
         var j : Int = 0
         var m : Int = 0
         var x : Int = 0
         var remainingSize = 0
-        statefulStream.flatMapWithState((in, state: Option[(Long,Long,Long,mutable.ArrayBuffer[T],mutable.ArrayBuffer[T],mutable.ArrayBuffer[T])]) => {
-
-
+        statefulStream.flatMapWithState((in, state: Option[inp[T]]) => {
           val (element,_)=in
           state match {
-            case Some(curr) => 
-              var (streamCounter1,streamCounter2,streamCounter3,adaptedReserS1,adaptedReserS2,adaptivereservoir) = curr
+            case Some(curr) => {
+              val mini_reservoir = curr
               var ret: Seq[mutable.ArrayBuffer[T]] = Seq()
-              if (streamCounter1 < k) {
-                adaptivereservoir(streamCounter1.toInt) =element
-                streamCounter1 += 1
-                streamCounter2+=1}
-              else if (streamCounter1 == nboCh*k)
+              if (mini_reservoir.streamCounter1 < k) {
+                mini_reservoir.adaptivereservoir(mini_reservoir.streamCounter1.toInt) =element
+                mini_reservoir.streamCounter1 += 1
+                mini_reservoir.streamCounter2+=1}
+              else if (mini_reservoir.streamCounter1 == nboCh*k)
                 {
-                  if (streamCounter1==streamCounter2)
+                  if (mini_reservoir.streamCounter1==mini_reservoir.streamCounter2)
                   {
                     val  delta = changedReservoirSize - k
                     if (delta <= 0) {
-                      adaptivereservoir =  Random.shuffle(adaptivereservoir.toList).take(changedReservoirSize).to[mutable.ArrayBuffer]
-                      streamCounter1+=1
-                      streamCounter2+=1
+                      mini_reservoir.adaptivereservoir =  Random.shuffle(mini_reservoir.adaptivereservoir.toList).take(changedReservoirSize).to[mutable.ArrayBuffer]
+                      mini_reservoir.streamCounter1+=1
+                      mini_reservoir.streamCounter2+=1
+
                     }else{
-                      m = min_m(streamCounter1.toInt,k,delta,thresh)
+                      m = min_m(mini_reservoir.streamCounter1.toInt,k,delta,thresh)
                       if(m==delta){
                         x = changedReservoirSize - m
                       }else {
                         x = changedReservoirSize - m + gen.nextInt(m-delta)
                       }
                       remainingSize = changedReservoirSize - x
-                      adaptedReserS1 = Random.shuffle(adaptivereservoir).take(x)
-                      adaptedReserS2 = mutable.ArrayBuffer.fill[T](remainingSize)(null.asInstanceOf[T])
-                      adaptedReserS2(0) = element
-                      streamCounter2+=1
+                      mini_reservoir.adaptedReserS1 = Random.shuffle(mini_reservoir.adaptivereservoir).take(x)
+                      mini_reservoir.adaptedReserS2 = mutable.ArrayBuffer.fill[T](remainingSize)(null.asInstanceOf[T])
+                      mini_reservoir.adaptedReserS2(0) = element
+                      mini_reservoir.streamCounter2+=1
                     }
                   }else{
-                    if (streamCounter2 < streamCounter1 + remainingSize){
-                      adaptedReserS2(streamCounter3.toInt) = element
-                      streamCounter3 += 1
-                      streamCounter2+=1
+                    if (mini_reservoir.streamCounter2 < mini_reservoir.streamCounter1 + remainingSize){
+                      mini_reservoir.adaptedReserS2(mini_reservoir.streamCounter3.toInt) = element
+                      mini_reservoir.streamCounter3 += 1
+                      mini_reservoir.streamCounter2+=1
                     } else {
                       j = gen.nextInt(m)
                       if (j < remainingSize)
-                        adaptedReserS2(j)  = element
-                      streamCounter2+=1
+                        mini_reservoir.adaptedReserS2(j)  = element
+                        mini_reservoir.streamCounter2+=1
                     }
-                    adaptivereservoir = adaptedReserS1 ++ adaptedReserS2
-                    if (streamCounter2==streamCounter1+m){ streamCounter1+=m}
+                    mini_reservoir.adaptivereservoir = mini_reservoir.adaptedReserS1 ++ mini_reservoir.adaptedReserS2
+                    if (mini_reservoir.streamCounter2==mini_reservoir.streamCounter1+m){ mini_reservoir.streamCounter1+=m}
                   }
                   k = changedReservoirSize
                 }else {
-                  j = gen.nextInt(streamCounter1.toInt)
+                  j = gen.nextInt(mini_reservoir.streamCounter1.toInt)
                   if (j < k)
-                    if(adaptivereservoir.length<=j) {
-                      adaptivereservoir=adaptivereservoir++mutable.ArrayBuffer.fill[T](j-adaptivereservoir.length+1)(null.asInstanceOf[T])
-                      adaptivereservoir(j) = element
+                    if(mini_reservoir.adaptivereservoir.length<=j) {
+                      mini_reservoir.adaptivereservoir=mini_reservoir.adaptivereservoir++mutable.ArrayBuffer.fill[T](j-mini_reservoir.adaptivereservoir.length+1)(null.asInstanceOf[T])
+                      mini_reservoir.adaptivereservoir(j) = element
                     }
-                  streamCounter1 += 1
-                  streamCounter2 +=1
+                mini_reservoir.streamCounter1 += 1
+                mini_reservoir.streamCounter2 +=1
                 }
-              ret = Seq(adaptivereservoir.filter(i=>i != null))
-              (ret, Some(streamCounter1,streamCounter2 ,streamCounter3,adaptedReserS1,adaptedReserS2, adaptivereservoir))
+              ret = Seq(mini_reservoir.adaptivereservoir.filter(i=>i != null))
+              (ret, Some(mini_reservoir))
               }
             case None => {
               var adaptivereservoir = mutable.ArrayBuffer.fill[T](k)(null.asInstanceOf[T])
               var adaptedReserS1 = mutable.ArrayBuffer.fill[T](0)(null.asInstanceOf[T])
               var adaptedReserS2 = mutable.ArrayBuffer.fill[T](0)(null.asInstanceOf[T])
               adaptivereservoir(0)=element
-              (Seq(adaptivereservoir.slice(0,1)),Some(1L,1L,1L,adaptedReserS1,adaptedReserS2,adaptivereservoir))
+              (Seq(adaptivereservoir.slice(0,1)),Some(inp(1L,1L,1L,adaptedReserS1,adaptedReserS2,adaptivereservoir)))
             }
           }
         }
