be4e835c8ebf36f9507e284b70a92fe460abe8f6
Ventura Del Monte <venturadelmonte@gmail.com>
2017-11-10 10:59:07 +0100

    Make flink ps build script work in any dir

--- scripts/build-flink-ps.sh
+++ scripts/build-flink-ps.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+BASE_DIR=$(dirname $0)/..
+
 git submodule update --init --recursive
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then 
-	sed -i 's/addSbtPlugin(\"com.eed3si9n\" \% \"sbt-assembly\" \% \"0.14.3\")/addSbtPlugin(\"com.eed3si9n\" \% \"sbt-assembly\" % \"0.14.5\")/g' external/flink-parameter-server/project/assembly.sbt
-	grep -q isSnapshot external/flink-parameter-server/build.sbt || sed -i 's/organization := \"hu.sztaki.ilab\"\,/organization := \"hu.sztaki.ilab\"\, isSnapshot := true,/g' external/flink-parameter-server/build.sbt
+	sed -i 's/addSbtPlugin(\"com.eed3si9n\" \% \"sbt-assembly\" \% \"0.14.3\")/addSbtPlugin(\"com.eed3si9n\" \% \"sbt-assembly\" % \"0.14.5\")/g' $BASE_DIR/external/flink-parameter-server/project/assembly.sbt
+	grep -q isSnapshot $BASE_DIR/external/flink-parameter-server/build.sbt || sed -i 's/organization := \"hu.sztaki.ilab\"\,/organization := \"hu.sztaki.ilab\"\, isSnapshot := true,/g' $BASE_DIR/external/flink-parameter-server/build.sbt
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-	sed -i ".sbt" 's/addSbtPlugin(\"com.eed3si9n\" \% \"sbt-assembly\" \% \"0.14.3\")/addSbtPlugin(\"com.eed3si9n\" \% \"sbt-assembly\" % \"0.14.5\")/g' external/flink-parameter-server/project/assembly.sbt
-	grep -q isSnapshot external/flink-parameter-server/build.sbt || sed -i ".sbt" 's/organization := \"hu.sztaki.ilab\"\,/organization := \"hu.sztaki.ilab\"\, isSnapshot := true,/g' external/flink-parameter-server/build.sbt
+	sed -i ".sbt" 's/addSbtPlugin(\"com.eed3si9n\" \% \"sbt-assembly\" \% \"0.14.3\")/addSbtPlugin(\"com.eed3si9n\" \% \"sbt-assembly\" % \"0.14.5\")/g' $BASE_DIR/external/flink-parameter-server/project/assembly.sbt
+	grep -q isSnapshot $BASE_DIR/external/flink-parameter-server/build.sbt || sed -i ".sbt" 's/organization := \"hu.sztaki.ilab\"\,/organization := \"hu.sztaki.ilab\"\, isSnapshot := true,/g' $BASE_DIR/external/flink-parameter-server/build.sbt
 else
 	echo "Unsupported OS for sed - please try to extend this script"
 	exit 1
 fi
-cd external/flink-parameter-server
+cd $BASE_DIR/external/flink-parameter-server
 sbt clean publishM2
