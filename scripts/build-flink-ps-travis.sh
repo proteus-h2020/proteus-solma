10f54977732e280540c803dd1793673371f2d88b
Ventura Del Monte <venturadelmonte@gmail.com>
2018-02-12 10:40:27 +0100

    Bump Flink to 1.4.0

--- scripts/build-flink-ps-travis.sh
+++ scripts/build-flink-ps-travis.sh
@@ -7,7 +7,7 @@ echo "building Flink PS on $TRAVIS_OS_NAME at $BASE_DIR"
 
 git submodule update --init --recursive
 
-sed -i 's/lazy val flinkVersion = \"1.2.0\"/lazy val flinkVersion = \"1.3.2\"/g' $BASE_DIR/external/flink-parameter-server/build.sbt
+sed -i 's/lazy val flinkVersion = \"1.2.0\"/lazy val flinkVersion = \"1.4.0\"/g' $BASE_DIR/external/flink-parameter-server/build.sbt
 sed -i 's/addSbtPlugin(\"com.eed3si9n\" \% \"sbt-assembly\" \% \"0.14.3\")/addSbtPlugin(\"com.eed3si9n\" \% \"sbt-assembly\" % \"0.14.5\")/g' $BASE_DIR/external/flink-parameter-server/project/assembly.sbt
 grep -q isSnapshot $BASE_DIR/external/flink-parameter-server/build.sbt || sed -i 's/organization := \"hu.sztaki.ilab\"\,/organization := \"hu.sztaki.ilab\"\, \
 isSnapshot := true,/g' $BASE_DIR/external/flink-parameter-server/build.sbt
