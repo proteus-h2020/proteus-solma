071c1f3c9698b9fcf1f18549d162313f84a99855
WenjuanW <wwang.elsie@gmail.com>
2018-07-11 13:45:53 +0100

    initialize parameter xp

--- src/test/scala/eu/proteus/solma/obsg_svm/OBSG_SVMITSuite.scala
+++ src/test/scala/eu/proteus/solma/obsg_svm/OBSG_SVMITSuite.scala
@@ -132,7 +132,7 @@ object OBSG_SVMITSuite {
     env.setParallelism(4)
 
     val obsg_svm = OBSG_SVM()
-      .setCParam(1)
+      .setCParam(0.1)
       .setFeaturesCount(8)
       .setPullLimit(10000)
       .setIterationWaitTime(20000)
